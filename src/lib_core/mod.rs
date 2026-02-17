f612b13b5833463e4007efc5f4fc0b2a6e82b48e
t.yamakawa99339 <t.yamakawa99339@gmail.com>
2026-02-17 22:01:41 +0900

    Rayクラスを追加

--- src/lib_core/mod.rs
+++ src/lib_core/mod.rs
@@ -1,3 +1,4 @@
 pub mod color;
 pub mod point;
+pub mod ray;
 pub mod vec;
---
+++ src/lib_core/ray.rs
@@ -0,0 +1,28 @@
+use crate::lib_core::point::Point3;
+use crate::lib_core::vec::Vec3;
+
+pub struct Ray {
+    org: Point3,
+    dir: Vec3,
+}
+
+impl Ray {
+    pub fn new(origin: Point3, direction: Vec3) -> Self {
+        Self {
+            org: origin,
+            dir: direction,
+        }
+    }
+
+    pub fn origin(&self) -> Point3 {
+        self.org
+    }
+
+    pub fn direction(&self) -> Vec3 {
+        self.dir
+    }
+
+    pub fn at(&self, t: f64) -> Point3 {
+        self.org + self.dir * t
+    }
+}
