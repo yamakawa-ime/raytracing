1b0056ed2b8e3aed15392063c0d0ca8db9502981
t.yamakawa99339 <t.yamakawa99339@gmail.com>
2026-02-19 21:28:33 +0900

    法線ベクトルを描画

--- src/main.rs
+++ src/main.rs
@@ -38,18 +38,26 @@ fn main() {
     println!("\nDone.");
 }
 
-fn hit_sphere(center: Point3, radius: f64, r: &Ray) -> bool {
+fn hit_sphere(center: Point3, radius: f64, r: &Ray) -> f64 {
     let oc = r.origin() - center;
     let a = r.direction().dot(r.direction());
     let b = 2.0 * oc.dot(r.direction());
     let c = oc.dot(oc) - radius * radius;
     let discriminant = b * b - 4.0 * a * c;
-    discriminant >= 0.0
+
+    if discriminant < 0.0 {
+        return -1.0;
+    } else {
+        return (-b - discriminant.sqrt()) / (2.0 * a);
+    }
 }
 
 fn ray_color(r: Ray) -> Color {
-    if hit_sphere(Point3::new(0.0, 0.0, -1.0), 0.5, &r) {
-        return Color::new(1.0, 0.0, 0.0);
+    let center = Vec3::new(0.0, 0.0, -1.0);
+    let t = hit_sphere(center, 0.5, &r);
+    if t > 0.0 {
+        let n = (r.at(t) - center).unit_vector();
+        return Color::new(n.x() + 1.0, n.y() + 1.0, n.z() + 1.0) * 0.5;
     }
 
     let unit_direction = r.direction().unit_vector();
