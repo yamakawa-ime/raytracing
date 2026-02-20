9fc6d75146743c9633f4f0cdfacdc1e4c07bafd8
t.yamakawa99339 <t.yamakawa99339@gmail.com>
2026-02-20 22:51:09 +0900

    SphereやHittableなどを追加

---
+++ src/lib_core/hittable.rs
@@ -0,0 +1,23 @@
+use crate::lib_core::{point::Point3, ray::Ray, vec::Vec3};
+
+pub struct HitRecord {
+    pub p: Point3,
+    pub normal: Vec3,
+    pub t: f64,
+    pub front_face: bool,
+}
+
+impl HitRecord {
+    pub fn set_face_normal(&mut self, r: &Ray, outward_normal: Vec3) {
+        self.front_face = r.direction().dot(outward_normal) < 0.0;
+        self.normal = if self.front_face {
+            outward_normal
+        } else {
+            -outward_normal
+        }
+    }
+}
+
+pub trait Hittable {
+    fn hit(&self, r: &Ray, ray_tmin: f64, ray_tmax: f64, rec: &mut HitRecord) -> bool;
+}
--- src/lib_core/mod.rs
+++ src/lib_core/mod.rs
@@ -1,4 +1,7 @@
 pub mod color;
+pub mod hittable;
+pub mod hittable_list;
 pub mod point;
 pub mod ray;
+pub mod sphere;
 pub mod vec;
