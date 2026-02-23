b63a75cf768c4ccdf9511c4c55ec4380b3b33a10
t.yamakawa99339 <t.yamakawa99339@gmail.com>
2026-02-23 21:28:47 +0900

    lambertian reflect

--- src/lib_core/camera.rs
+++ src/lib_core/camera.rs
@@ -108,7 +108,7 @@ impl Camera {
         }
 
         if world.hit(&r, Interval::new(0.001, f64::INFINITY), &mut rec) {
-            let direction = Vec3::random_on_hemisphere(rec.normal);
+            let direction = rec.normal + Vec3::random_unit_vector();
             return self.ray_color(Ray::new(rec.p, direction), depth - 1, world) * 0.5;
         }
 
