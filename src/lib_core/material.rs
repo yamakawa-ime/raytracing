7b1546718e804a676eb204c553676c693b54df22
t.yamakawa99339 <t.yamakawa99339@gmail.com>
2026-02-27 23:51:16 +0900

    HollowGlass

--- src/lib_core/material.rs
+++ src/lib_core/material.rs
@@ -1,4 +1,6 @@
-use crate::lib_core::{color::Color, hittable::HitRecord, ray::Ray, vec::Vec3};
+use crate::lib_core::{
+    color::Color, hittable::HitRecord, ray::Ray, rtweekend::random_double, vec::Vec3,
+};
 
 pub trait Material {
     fn scatter(
@@ -79,6 +81,12 @@ impl Dielectric {
             ir: index_of_refraction,
         }
     }
+
+    fn reflectance(cosine: f64, ref_idx: f64) -> f64 {
+        let mut r0 = (1.0 - ref_idx) / (1.0 + ref_idx);
+        r0 = r0 * r0;
+        r0 + (1.0 - r0) * (1.0 - cosine).powi(5)
+    }
 }
 
 impl Material for Dielectric {
@@ -100,7 +108,9 @@ impl Material for Dielectric {
         let sin_theta = (1.0 - cos_theta * cos_theta).sqrt();
 
         let cannot_refract = refraction_ratio * sin_theta > 1.0;
-        let direction = if cannot_refract {
+        let direction = if cannot_refract
+            || Dielectric::reflectance(cos_theta, refraction_ratio) > random_double()
+        {
             unit_direction.reflect(rec.normal)
         } else {
             unit_direction.reflact(rec.normal, refraction_ratio)
--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,8 @@ fn main() {
     let material_ground = Rc::new(Lambertian::new(Color::new(0.8, 0.8, 0.0)));
     let material_center = Rc::new(Lambertian::new(Color::new(0.1, 0.2, 0.5)));
     let material_left = Rc::new(Dielectric::new(1.5));
-    let material_right = Rc::new(Metal::new(Color::new(0.8, 0.6, 0.2), 1.0));
+    let material_bubble = Rc::new(Dielectric::new(1.00 / 1.50));
+    let material_right = Rc::new(Metal::new(Color::new(0.8, 0.6, 0.2), 0.0));
 
     world.add(Rc::new(Sphere::new(
         Point3::new(0.0, -100.5, -1.0),
@@ -32,6 +33,11 @@ fn main() {
         Point3::new(-1.0, 0.0, -1.0),
         0.5,
         material_left,
+    )));
+    world.add(Rc::new(Sphere::new(
+        Point3::new(-1.0, 0.0, -1.0),
+        0.4,
+        material_bubble,
     )));
     world.add(Rc::new(Sphere::new(
         Point3::new(1.0, 0.0, -1.0),
