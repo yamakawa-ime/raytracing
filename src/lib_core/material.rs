dced663d03fb7cb95566f93f27c9e4a72965c949
t.yamakawa99339 <t.yamakawa99339@gmail.com>
2026-02-27 23:23:32 +0900

    全反射実装

--- src/lib_core/material.rs
+++ src/lib_core/material.rs
@@ -96,8 +96,16 @@ impl Material for Dielectric {
             self.ir
         };
         let unit_direction = r_in.direction().unit_vector();
-        let refracted = unit_direction.reflact(rec.normal, refraction_ratio);
-        *scattered = Ray::new(rec.p, refracted);
+        let cos_theta = (-unit_direction).dot(rec.normal).min(1.0);
+        let sin_theta = (1.0 - cos_theta * cos_theta).sqrt();
+
+        let cannot_refract = refraction_ratio * sin_theta > 1.0;
+        let direction = if cannot_refract {
+            unit_direction.reflect(rec.normal)
+        } else {
+            unit_direction.reflact(rec.normal, refraction_ratio)
+        };
+        *scattered = Ray::new(rec.p, direction);
         true
     }
 }
